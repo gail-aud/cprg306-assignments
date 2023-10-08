376d9d324b58f9a5d19d5e9f54edb16d2e02cba2
gail-aud <gail.javier@edu.sait.ca>
2023-10-08 16:22:24 -0600

    week 4 assignments
    update/fixing/testing

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
       <div>
-        <p>Name: Gail Audrey Javier</p>
+        <h1>Name: Gail Audrey Javier</h1>
         <p> Course Section: CPRG 306A</p>
         <Link href={"https://github.com/gail-aud/cprg306-assignments"} className="hover:text-blue-200">Github Repository</Link>
       </div>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import StudentInfo from "./StudentInfo";
 import Link from "next/link";
 
-export default function Home() {
+export default function Page() {
 
   return (
     <main className="flex min-h-screen flex-col items-center pt-24">
@@ -9,6 +9,7 @@ export default function Home() {
       <StudentInfo />
       <Link href={`/week2`} className="hover:text-blue-200 text-sm "> Week 2 </Link>
       <Link href={`/week3`} className="hover:text-blue-200 text-sm "> Week 3 </Link>
+      <Link href={`/week4`} className="hover:text-blue-200 text-sm "> Week 4 </Link>  
     </main>
   );
 
