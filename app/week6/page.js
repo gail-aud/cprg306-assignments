710fa37bab58eb42e21a4a4cd8c34d6a06c51ab2
gail-aud <gail.javier@edu.sait.ca>
2023-10-23 23:01:25 -0600

    Update page.js

--- app/week6/page.js
+++ app/week6/page.js
@@ -18,7 +18,8 @@ export default function Page () {
     return(
         <main>
             <h1 className="text-3xl font-bold m-2 text-blue-200">Shopping List</h1>
-            <ItemList />
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList items={items} />
         </main>
     );
 }
